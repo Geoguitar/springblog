5978943a5f0948202c83aabb0721d0a77299e623
Geoguitar <geovanedsilveira@gmail.com>
2024-01-27 04:23:11 -0300

    Implemented controller class

--- src/main/java/com/geodeveloper/springblog/controller/ArtigoController.java
+++ src/main/java/com/geodeveloper/springblog/controller/ArtigoController.java
@@ -1,4 +1,23 @@
 package com.geodeveloper.springblog.controller;
 
+import com.geodeveloper.springblog.model.Artigo;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/artigos")
 public class ArtigoController {
+    @GetMapping
+    public List<Artigo> obterTodos(){
+        return null;
+    }
+    @GetMapping("/{codigo}")
+    public Artigo obterPorCodigo(@PathVariable String codigo){
+        return null;
+    }
+    @PostMapping
+    public Artigo criar(@RequestBody Artigo artigo){
+        return null;
+    }
 }
