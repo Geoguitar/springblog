ad8c2c6676396e0cd10eb2d89d409b3d4534128c
Geoguitar <geovanedsilveira@gmail.com>
2024-01-27 04:10:43 -0300

    Implemented service

--- src/main/java/com/geodeveloper/springblog/service/ArtigoService.java
+++ src/main/java/com/geodeveloper/springblog/service/ArtigoService.java
@@ -1,4 +1,12 @@
 package com.geodeveloper.springblog.service;
 
+import com.geodeveloper.springblog.model.Artigo;
+
+import java.util.List;
+
 public interface ArtigoService {
+
+    public List<Artigo> obterTodos();
+    public Artigo obterPorCodigo(String codigo);
+    public Artigo criar(Artigo artigo);
 }
